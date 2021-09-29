73fba9c4d7eb40989044be6f200e845e511ae167
masatakamaekawa <mstkmekw@hito-koto.com>
2021-09-29 19:47:33 +0900

    課題zipcodエラー発生

---
+++ resources/views/customers/create.blade.php
@@ -0,0 +1,45 @@
+@extends('layouts.main')
+
+@section('title', '新規登録画面')
+
+@section('content')
+
+    @if ($errors->any())
+        <div class="error">
+            <p>
+                <b>{{ count($errors) }}件のエラーがあります。</b>
+            </p>
+            <ul>
+                @foreach($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+    <h1>新規登録画面</h1>
+    <form action="{{ route('customers.store') }}" method="POST">
+        @csrf
+        <div>
+            <label for="name">名前</label>
+            <input type="text" name="name" id="name" value="{{ old('name') }}" >
+        </div>
+        <div>
+            <label for="email">メールアドレス</label>
+            <input type="text" name="email" id="email" value="{{ old('email') }}" >
+        </div>
+        <div>
+            <label for="zipcode">郵便番号</label>
+            <input type="text" name="zipcode" id="zipcode"  value="{{ old('zipcode', $zipcode)}}">
+        </div>
+        <div>
+            <label for="address">住所</label>
+            <textarea name="address" id="address" cols="30" rows="10" >{{ $address }}</textarea>
+        </div>
+        <div>
+            <label for="tel">電話番号</label>
+            <input type="text" name="phone" id="phone" value="{{ old('phone') }}" >
+        </div>
+        <input type="submit" value="登録">
+    </form>
+    <button onclick="location.href='{{ route('customers.zipcode') }}'">郵便番号検索に戻る</button>
+@endsection
